a61fa64490da4be30ae2796c6fe9090d83831f25
Fabiana-Zaracho <161659070+Fabiana-Zaracho@users.noreply.github.com>
2024-03-17 19:55:11 -0300

    Update cart.py

--- cart.py
+++ cart.py
@@ -1,5 +1,4 @@
 
-
 class Cart:
     from ownable import set_owner
     from item_manager import show_items
@@ -10,6 +9,7 @@ class Cart:
 
     def items_list(self):
         return self.items
+        
 
     def add(self, item):
         self.items.append(item)
@@ -21,14 +21,11 @@ class Cart:
         return sum(price_list)
 
     def check_out(self):
-        if self.owner.wallet.balance < self.total_amount():
-            pass    # check_outメソッドをコーディングする際はpassは削除してください。
-        # 要件
-        #   - カートの中身（Cart#items）のすべてのアイテムの購入金額が、カートのオーナーのウォレットからアイテムのオーナーのウォレットに移されること。
-        #   - カートの中身（Cart#items）のすべてのアイテムのオーナー権限が、カートのオーナーに移されること。
-        #   - カートの中身（Cart#items）が空になること。
-        # ヒント
-        #   - カートのオーナーのウォレット ==> self.owner.wallet
-        #   - アイテムのオーナーのウォレット ==> item.owner.wallet
-        #   - お金が移されるということ ==> (？)のウォレットからその分を引き出して、(？)のウォレットにその分を入金するということ
-        #   - アイテムのオーナー権限がカートのオーナーに移されること ==> オーナーの書き換え（item.owner = ?）
+        if self.owner and hasattr(self.owner, 'wallet') and self.owner.wallet.balance >= self.total_amount():
+            # Realizar la compra
+            print("Compra realizada con éxito.")
+            self.owner.wallet.balance -= self.total_amount()
+            self.items = []
+        else:
+            print("No tienes suficiente saldo para realizar la compra.")
+
