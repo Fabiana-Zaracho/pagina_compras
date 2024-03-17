23d8bd5ed96ae59094c2c4e8ce8c244f071b695b
Fabiana-Zaracho <161659070+Fabiana-Zaracho@users.noreply.github.com>
2024-03-17 20:03:40 -0300

    Update item_manager.py

--- item_manager.py
+++ item_manager.py
@@ -1,14 +1,17 @@
-# このモジュールをインクルードすると、自身の所有するItemインスタンスを操れるようになります。
+# Al incluir este módulo, podrás manipular las instancias de Item que posees.
+
 
 from item import Item
 from tabulate import tabulate
 from itertools import groupby
 
-def items_list(self):   # 自身の所有する（自身がオーナーとなっている）全てのItemインスタンスを返します。
+def items_list(self):   # Retorna todas las instancias de Item que posee (es decir, de las que es propietario).
+
     items = [item for item in Item.item_all() if item.owner == self]
     return items
 
-def pick_items(self, number, quantity):   # numberと対応した自身の所有するItemインスタンスを指定されたquantitiy分返します。
+def pick_items(self, number, quantity):   # Devuelve la instancia de Item correspondiente al número especificado, junto con la cantidad especificada, que posee.
+
     items = filter(lambda num: num["number"] == number, _stock(self))
     items = list(items)
     if len(items) == 0:
@@ -18,17 +21,21 @@ def pick_items(self, number, quantity):   # numberと対応した自身の所有
     else:
         return items[0]["items"][0:quantity]
 
-def show_items(self):   # 自身の所有するItemインスタンスの在庫状況を、["番号", "商品名", "金額", "数量"]という列でテーブル形式にして出力します。
+def show_items(self):   # Imprime en forma de tabla el estado del inventario de las instancias de Item que posee, con columnas ["Número", "Nombre del producto", "Precio", "Cantidad"].
+
     table_data = []
     for stock in _stock(self):
         table_data.append([stock['number'], stock['label']['name'], stock['label']['price'], len(stock['items'])])
-    print(tabulate(table_data, headers=["番号", "商品名", "金額", "数量"], tablefmt="grid"))    # tabulateモジュールを使ってテーブル形式で結果を出力
+    print(tabulate(table_data, headers=["Número", "Nombre del producto", "Precio", "Cantidad"], tablefmt="grid"))    # Imprimir los resultados en formato de tabla utilizando el módulo tabulate.
 
-def _stock(self):   # 自身の所有するItemインスタンスの在庫状況を返します。
+
+def _stock(self):   # Retorna el estado del inventario de las instancias de Item que posee.
+
     item_ls = self.items_list()
     item_ls.sort(key=lambda m: m.name)
     group_list = []
-    for key, group in groupby(item_ls, key=lambda m: m.name):   # Item#nameで同じ値を返すItemインスタンスで分類します。
+    for key, group in groupby(item_ls, key=lambda m: m.name):   # Clasifica las instancias de Item que devuelven el mismo valor con Item#name.
+
         group_list.append(list(group))
     stock = []
     for index, item in enumerate(group_list):
